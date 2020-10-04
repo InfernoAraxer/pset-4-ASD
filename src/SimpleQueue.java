9d2cceff1a3c2f4725498c45a5ea776a17c0133f
Alex Tan <AT212355@gmail.com>
2020-10-04 15:40:44 -0400

    Finished

--- src/SimpleQueue.java
+++ src/SimpleQueue.java
@@ -1,24 +1,19 @@
-import java.util.List;
+import java.util.NoSuchElementException;
 
 public class SimpleQueue {
 	
 	public static String[] data;
 	public static int size;
-	
-	public SimpleQueue() {
-		data = new String[1];
-		data[0] = "";
-		size = 0;
-	}
+	public static int capacity;
+	public String head = null;
+	public String tail = null;
+	public static int index = -1;
 	
 	public SimpleQueue(int initialCapacity) {
-		data = new String[1];
-		data[0] = "";
+		if (initialCapacity <= 0) throw new IllegalArgumentException();
+		data = new String[initialCapacity];
 		size = 0;
-	}
-	
-	public SimpleQueue(List<String> list) {
-		
+		capacity = initialCapacity;
 	}
 	
 	public static void main(String[] args) {
@@ -26,54 +21,126 @@ public class SimpleQueue {
 	}
 	
 	public boolean add(String s) {
-		
+		if (size == capacity) {
+			throw new IllegalStateException("Queue full");
+		}
+		if (s == null) throw new NullPointerException();
+		if (size == 0) head = s;
+		data[size] = s;
+		tail = s;
+		size++;
+		return true;
 	}
 	
 	public void clear() {
-		
+		data = new String[capacity];
+		size = 0;
 	}
-	
-	public String comparator() {
-		
-	}
-	
-	public contains(String s) {
-		
+
+	public boolean contains(String s) {
+		for (int x = 0; x < size; x++) {
+			if (data[x] == s) return true;
+			x++;
+		}
+		return false;
 	}
 	
 	public String element() {
-		
+		if (head == null) {
+			throw new NoSuchElementException();
+		} else {
+			return head;
+		}
 	}
 	
 	public boolean offer(String s) {
-		
+		if (size == capacity) {
+			return false;
+		}
+		if (s == null) throw new NullPointerException();
+		if (size == 0) head = s;
+		data[size] = s;
+		tail = s;
+		size++;
+		return true;
 	}
 	
 	public String peek() {
-		
+		return head;
 	}
 	
 	public String poll() {
-		
+		String poll = (size == 0) ? null : head;
+		if (poll == null) {
+			return poll;
+		} else {
+			String removed = data[0];
+			data[0] = null;
+			head = data[1];
+			removeExtraBackSpace();
+			return removed;
+		}
 	}
 	
 	public String remove() {
+		if (size == 0) throw new NoSuchElementException();
+		String removed = data[0];
+		data[0] = null;
+		head = data[1];
+		removeExtraBackSpace();
+		return removed;
+	}
+	
+	public boolean remove(String s) {
+		index = -1;
+		String[] temp = new String[capacity];
+		for (int x = 0; x < size; x++) {
+			if (data[x] == s) {
+				index = x;
+			}
+		}
+		if (index == -1) return false;
+		for (int y = 0; y < size; y++) {
+			if (y < index) {
+				temp[y] = data[y];
+			} else if (y > index) { 
+				temp[y - 1] = data[y];
+			}
+		}
+		data = temp;
+		size--;
+		return true;
 		
 	}
 	
-	public String remove(String s) {
-		
+	public int remainingCapacity() {
+		return capacity - size;
+	}
+	
+	public int size() {
+		return size;
 	}
 	
 	public String toString() {
 		String concat = "[";
-		for (int x = 0; x < data.length; x++) {
+		for (int x = 0; x < size - 1; x++) {
 			if (data[x] != null) {
-				concat += data[x];
-				concat += ", ";
+				concat += data[x] + ", ";
 			}
 		}
-		concat = concat.substring(0,concat.length()-2);
-		return concat += "]";
+		concat += (size != 0) ? data[size - 1] + "]" : "]" ;
+		return concat;
+	}
+
+
+	public void removeExtraBackSpace() {
+		String[] temp = new String[capacity];
+		for(int z = 0; z < size; z++) {
+			temp[z] = data[z + 1];
+		}
+		data = new String[capacity];
+		data = temp;
+		size--;
 	}
 }
+
